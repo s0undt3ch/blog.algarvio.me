db52d60092b6d147df6d788404db60fd7c0cce87
Pedro Algarvio <pedro@algarvio.me>
2015-01-17 01:13:10 +0000

    No relative URLs

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
