ccc341d5a1088b75c6e53ab4d234a52d2bdb2080
Pedro Algarvio <pedro@algarvio.me>
2015-06-29 10:56:53 +0100

    Minor URL fixes

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,14 +5,14 @@ import os
 
 AUTHOR = u'Pedro Algarvio, aka, s0undt3ch'
 SITENAME = u"s0undt3ch's Blog"
-SITEURL = 'http://blog.ufsoft.org/'
+SITEURL = 'http://blog.ufsoft.org'
 PYGMENTS_STYLE = 'fruity'
 CC_LICENSE = 'CC-BY-NC'
 THEME = '.pelican-bootstrap3-themes'
 BOOTSTRAP_THEME = 'darkly'
 #BOOTSTRAP_THEME = 'solarizedlight'
 GOOGLE_ANALYTICS = 'UA-253152-15'
-AVATAR = 'images/gravatar.275x275.png'
+AVATAR = '/images/gravatar.275x275.png'
 ABOUT_ME = 'Hacking Python For Fun!'
 CUSTOM_CSS = 'static/overrides.css'
 
@@ -22,7 +22,6 @@ STATIC_PATHS = ['images', 'css/overrides.css', 'CNAME']
 
 # Tell Pelican to change the path to 'static/overrides.css' in the output dir
 EXTRA_PATH_METADATA = {
-    'images': {'path': 'images'},
     'css/overrides.css': {'path': 'static/overrides.css'},
     'CNAME': {'path': 'CNAME'}
 }
@@ -69,7 +68,7 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 # Social widget
 SOCIAL = (
-    ('Linked-In', 'http://www.linkedin.com/in/pedroalgarvio'),
+    ('LinkedIn', 'http://www.linkedin.com/in/pedroalgarvio'),
     ('GitHub', 'https://github.com/s0undt3ch'),
     ('Bitbucket', 'https://bitbucket.org/s0undt3ch'),
     ('Open Hub - Ohloh', 'https://www.openhub.net/accounts/s0undt3ch'),
